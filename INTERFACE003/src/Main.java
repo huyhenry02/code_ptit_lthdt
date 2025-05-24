84253c0b49a45d456e4eaaac249cc5ac62e53294
Tran Duc Huy <sandbox.duchuy02@gmail.com>
2025-05-24 13:56:56 +0700

    day 2

---
+++ INTERFACE003/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        TacoBox tripleBox = new TripleTacoBox();
+        tripleBox.eat();
+        tripleBox.eat();
+        System.out.println("Triple taco boxes left: " + tripleBox.tacosRemaining());
+        
+        TacoBox customerBox = new CustomTacoBox(8);
+        customerBox.eat();
+        System.out.println("Custom taco boxes left: " + customerBox.tacosRemaining());
+    }
+}
+
+interface TacoBox {
+    int tacosRemaining();
+    void eat();
+}
+
+class TripleTacoBox implements TacoBox {
+    private int tacos;
+    
+    public TripleTacoBox(){
+        this.tacos = 3;
+    }
+    @Override
+    public void eat() {
+        if (tacos > 0){
+            tacos --;
+        }
+    }
+    
+    @Override
+    public int tacosRemaining() {
+        return tacos;
+    }
+}
+
+class CustomTacoBox implements TacoBox{
+    private int tacos;
+    
+    public CustomTacoBox(int initialTacos) {
+        this.tacos = initialTacos;
+    }
+    
+    @Override
+    public int tacosRemaining() {
+        return tacos;
+    }
+    
+    @Override
+    public void eat() {
+        if (tacos > 0) {
+            tacos--;
+        }
+    }
+}
---
+++ STRING006/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = Integer.parseInt(sc.nextLine());
+        
+        for (int i = 0; i < n; i++) {
+            String line = sc.nextLine();
+            StringBuilder initials = new StringBuilder();
+            
+            for (char c : line.toCharArray()) {
+                if (Character.isUpperCase(c)) {
+                    initials.append(c);
+                }
+            }
+            
+            System.out.println("My initials are: " + initials);
+        }
+        
+        sc.close();
+    }
+}
